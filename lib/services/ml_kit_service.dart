e6c3b0ade38ff47b3a06d8ea83ccfd26c7d9398b
Furina (Michael Bello) <hidenoritabata755@gmail.com>
2026-02-06 18:00:37 +0800

    Yeah More API!!!

---
+++ lib/services/ml_kit_service.dart
@@ -0,0 +1,8 @@
+import 'package:google_mlkit_text_recognition/google_mlkit_text_recognition.dart';
+import 'package:google_mlkit_genai_image_description/google_mlkit_genai_image_description.dart';
+import 'package:google_mlkit_genai_prompt/google_mlkit_genai_prompt.dart';
+import 'package:google_mlkit_object_detection/google_mlkit_object_detection.dart';
+
+// IDK hahaahah
+
+
